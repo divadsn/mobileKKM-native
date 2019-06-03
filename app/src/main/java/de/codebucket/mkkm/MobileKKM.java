b69cf007d6c2bb57a3c695c4267e259c3c7a370a
David Sn <divad.nnamtdeis@gmail.com>
2019-06-03 18:20:34 +0200

    Initial commit from Android Studio
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

---
+++ app/src/main/java/de/codebucket/mkkm/MobileKKM.java
@@ -0,0 +1,11 @@
+package de.codebucket.mkkm;
+
+import android.app.Application;
+
+public class MobileKKM extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
