cf039bd00b474e45c082779efa25c6451fd6dce7
David Sn <divad.nnamtdeis@gmail.com>
2019-06-13 10:59:05 +0200

    Add redesigned overview activity
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

---
+++ app/src/main/java/de/codebucket/mkkm/ui/MainActivity.java
@@ -0,0 +1,30 @@
+package de.codebucket.mkkm.ui;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import de.codebucket.mkkm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+}
