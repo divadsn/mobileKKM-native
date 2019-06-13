435e225dd96023a3b9aca20fac0881a6f7ec2173
David Sn <divad.nnamtdeis@gmail.com>
2019-06-13 11:03:45 +0200

    Open overview after splash screen for now
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- app/src/main/java/de/codebucket/mkkm/ui/SplashActivity.java
+++ app/src/main/java/de/codebucket/mkkm/ui/SplashActivity.java
@@ -1,7 +1,8 @@
 package de.codebucket.mkkm.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,26 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                launch(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 500L);
+    }
+
+    private void launch(final Intent intent) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        });
     }
 }
