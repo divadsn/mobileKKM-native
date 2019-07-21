dd4b966adb67dd3cf8b2e15236c42f95a143ab5a
David Sn <divad.nnamtdeis@gmail.com>
2019-07-21 02:41:58 +0200

    Rework main activity layout for custom toolbar
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- app/src/main/java/de/codebucket/mkkm/ui/MainActivity.java
+++ app/src/main/java/de/codebucket/mkkm/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
